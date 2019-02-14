4a648b591b2e7c0a3b2d176a3f846fe12be2ee70
xttjsn <xttjsn@gmail.com>
2019-02-14 15:54:44 -0500

    Add echo command to shell

--- shell.py
+++ shell.py
@@ -3,61 +3,77 @@ The shell script
 """
 import os
 import sys
-from cmd2 import Cmd
+from cmd import Cmd
+
 
 BLKSZ = 1024
 
 class TradingShell(Cmd):
 
     prompt = '>>>'
-    colors = 'Always'
     def __init__(self):
         super(TradingShell, self).__init__()
         self.backtest_engines = ['zipline']
         self.actions = {
-            'backtest': self.backtest
+            'backtest': self.backtest,
+            'echo': self.echo
         }
 
-    def parse(self, line, command):
-        # backtest zipline ./algo.py
-
-    def do_any(self, arg):
-        arg = arg.lstrip().rstrip()
-        if arg == '':
-            return
-
+    def piperun(self, arg):
         piped_args = arg.split('|')
-
-        rend = sys.stdin
+        r = None
         for arg in piped_args:
             args = arg.split()
             if len(args) == 0:
-                self.perror('Invalid args. | with spaces, really?')
+                self.perror('Invalid args')
                 return
 
             command = args[0]
 
-            newrend, wend = os.pipe()
+            rr, w = os.pipe()
             try:
-                output = self.actions[command](self.parse(arg[1:]),
-                                               stdin=rend,
-                                               stdout=wend)
-                rend = newrend
+                self.actions[command](' '.join(args[1:]),
+                                      inPipe=r,
+                                      outPipe=w)
+                r = rr
 
             except Exception as e:
                 self.perror(e)
 
-        while True:
-            buf = os.read(newrend, BLKSZ)
-            self.poutput(buf.encode('uft-8'))
+        self.writeTo(1, self.readFrom(r) + b'\n')
+        
+    def readFrom(self, pipe):
+        # print('Reading from {}'.format(pipe))
+        buf = b''
+        while True and pipe:
+            buf_more = os.read(pipe, BLKSZ)
+            if not buf_more:
+                break
+            buf += buf_more
 
+        # print("reading: {}".format(buf))
+        return buf
+
+    def writeTo(self, pipe, buf):
+        os.write(pipe, buf)
+
+        
+    def do_echo(self, arg):
+        self.piperun('echo ' + arg)
+
+    def echo(self, arg, inPipe=None, outPipe=1):
+        buf = self.readFrom(inPipe)    
+        self.writeTo(outPipe, buf + arg.encode('utf-8') )
+        os.close(outPipe)
+        return
+           
     def do_b(self, arg):
         self.do_backtest(arg)
         
     def do_backtest(self, arg):
-        self.do_any('backtest ' + arg)
+        self.piperun('backtest ' + arg)
 
-    def backtest(self, engine=zipline, algofile='./algo.py'):
+    def backtest(self, engine='zipline', algofile='./algo.py'):
         
         if args[0] not in self.backtest_engines:
             self.perror('{} not a valid backtest engine'.format(args[0]))
